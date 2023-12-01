c59f5e82a9bc815f529abfb59a2cd862378fc162
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-12-01 10:38:51 +0100

    modified

--- src/components/pages/studentDashboard/StudentDashboard.js
+++ src/components/pages/studentDashboard/StudentDashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./StudentDashboard.css"
 const StudentDashboard = () => {
   return (
-    <div className='studentDashboard-wrapper'>StudentDashboard Student</div>
+    <div className='studentDashboard-wrapper'>StudentDashboard</div>
   )
 }
 
