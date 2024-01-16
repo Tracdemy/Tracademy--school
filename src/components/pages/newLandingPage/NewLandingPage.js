6452c8b7bb14ab931e2884b3f32016d2eadbbe72
Michael Adeshina <adeshinaobafemi09@gmail.com>
2024-01-17 00:13:56 +0100

    feat: NewLandingPage styled

--- src/components/pages/newLandingPage/NewLandingPage.js
+++ src/components/pages/newLandingPage/NewLandingPage.js
@@ -3,29 +3,29 @@ import image1 from "./assets/Fileimage1.png";
 import { Link } from "react-router-dom";
 import image2 from "./assets/cuateimage2.png";
 import logo from "./assets/Group 67logo.png";
-
+import "./NewLandingPage.css"
 const NewLandingPage = () => {
   return (
     <div>
-      <div className="flex flex-col items-center justify-between">
-        <div className="flex items-end justify-between">
+      <div className=" newLandingPage-wrapper flex flex-col items-center">
+        <div className="newLandingPage-contents flex items-end justify-between">
         <div className="flex flex-col">
-          <p>Your schools best option</p>
-          <p>FOR SAFE RECORD MANAGEMENT</p>
+          <p className="schools-option">Your <span className="schools">schools best option</span></p>
+          <p className="schools-management">FOR SAFE RECORD MANAGEMENT</p>
           </div>
           <img src={image1} alt="img" className="" />
         </div>
-        <div className="flex items-start justify-between">
+        <div className="startsHere flex items-start justify-between">
           <div className="flex flex-col items-center justify-between">
-            <Link>Starts Here</Link>
+            <Link className="starts">Starts Here</Link>
             <img src={image2} alt="img" className="" />
           </div>
           <div>
-            <img src={logo} alt="img" className="" />
+            <img src={logo} alt="img" className="logo" />
             <div className="flex flex-col">
-              <div className="flex items-center justify-between">
-                <button className="px-5 py-1 rounded-lg">Sign In</button>
-                <button className="px-5 py-1 rounded-lg">Sign Up</button>
+              <div className="btnsFlex flex items-center justify-between">
+                <button className="btns px-5 rounded-lg">Sign In</button>
+                <button className="btns px-5 rounded-lg">Sign Up</button>
               </div>
               <div>Two</div>
             </div>
