5262e0b06472679b7e00c82fa0c830b376e95c8c
Michael Adeshina <adeshinaobafemi09@gmail.com>
2024-01-17 00:46:54 +0100

    style: NewLandingPage media queries

--- src/components/pages/newLandingPage/NewLandingPage.js
+++ src/components/pages/newLandingPage/NewLandingPage.js
@@ -9,25 +9,24 @@ const NewLandingPage = () => {
     <div>
       <div className=" newLandingPage-wrapper flex flex-col items-center">
         <div className="newLandingPage-contents flex items-end justify-between">
-        <div className="flex flex-col">
+        <div className="best flex flex-col">
           <p className="schools-option">Your <span className="schools">schools best option</span></p>
           <p className="schools-management">FOR SAFE RECORD MANAGEMENT</p>
           </div>
-          <img src={image1} alt="img" className="" />
+          <img src={image1} alt="img" className="image1" />
         </div>
         <div className="startsHere flex items-start justify-between">
           <div className="flex flex-col items-center justify-between">
             <Link className="starts">Starts Here</Link>
             <img src={image2} alt="img" className="" />
           </div>
-          <div>
+          <div className="flex flex-col items-center">
             <img src={logo} alt="img" className="logo" />
             <div className="flex flex-col">
-              <div className="btnsFlex flex items-center justify-between">
-                <button className="btns px-5 rounded-lg">Sign In</button>
-                <button className="btns px-5 rounded-lg">Sign Up</button>
+              <div className="btnsFlex flex items-b justify-between">
+                <button className="btns px-5 rounded-lg" id="btns1">Sign In</button>
+                <button className="btns px-5 rounded-lg" id="btns2">Sign Up</button>
               </div>
-              <div>Two</div>
             </div>
           </div>
         </div>
